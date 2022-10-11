3490920056d59298697e1b43e0e2a554646ff2d8
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-10-11 22:07:06 +0300

    correct

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,85 +1,23 @@
 const express = require("express");
 
 const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../../models/contacts");
+  getAllContacts,
+  getContact,
+  createContact,
+  deleteContact,
+  changeContact,
+} = require("../../controls/controls");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  const result = await listContacts();
-  res.status(200).json({
-    status: "success",
-    code: 201,
-    data: { result },
-  });
-});
+router.get("/", getAllContacts);
 
-router.get("/:contactId", async (req, res, next) => {
-  const result = await getContactById(req.params.contactId);
+router.get("/:contactId", getContact);
 
-  if (result.length === 0) {
-    res.status(404).json({ message: "Not found" });
-    return;
-  }
+router.post("/", createContact);
 
-  res.status(200).json(result);
-});
+router.delete("/:contactId", deleteContact);
 
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-
-  if (name && email && phone) {
-    const result = await addContact(req.body);
-
-    if (result.error) {
-      res.status(400).json(result);
-      return;
-    }
-
-    res.status(201).json(result);
-
-    return;
-  }
-
-  res.status(400).json({ message: "missing required name field" });
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  const result = await removeContact(req.params.contactId);
-
-  if (result) {
-    res.status(200).json({ message: "contact deleted" });
-
-    return;
-  }
-
-  res.status(404).json({ message: "Not found" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  if (Object.entries(req.body).length === 0) {
-    res.status(400).json({ message: "missing fields" });
-    return;
-  }
-
-  const result = await updateContact(req.params.contactId, req.body);
-
-  if (result.error) {
-    res.status(400).json(result);
-    return;
-  }
-
-  if (result) {
-    res.status(200).json(result);
-    return;
-  }
-
-  res.status(404).json({ message: "Not found" });
-});
+router.put("/:contactId", changeContact);
 
 module.exports = router;
