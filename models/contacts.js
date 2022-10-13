c92b703e162f9e7da8e3ac153368538ebecf35fb
Stanislav Sobolev <90769547+Stanislav-Sobolev@users.noreply.github.com>
2022-10-13 22:44:24 +0300

    Revert "Hw02 express"

--- models/contacts.js
+++ models/contacts.js
@@ -1,96 +1,14 @@
-const fs = require("fs/promises");
-const path = require("path");
-const db = require("./contacts.json");
-const dbPath = path.join(__dirname, "contacts.json");
-const Joi = require("joi");
+// const fs = require('fs/promises')
 
-const listContacts = async () => {
-  return await db;
-};
+const listContacts = async () => {}
 
-const getContactById = async (contactId) => {
-  const [result] = await db.filter((el) => el.id === contactId);
-  return result;
-};
+const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {
-  const idStringified = contactId.toString();
+const removeContact = async (contactId) => {}
 
-  const findIndexContact = await db.findIndex((el) => el.id === idStringified);
+const addContact = async (body) => {}
 
-  if (findIndexContact === -1) {
-    return null;
-  }
-
-  const [removedContact] = db.splice(findIndexContact, 1);
-  await fs.writeFile(dbPath, JSON.stringify(db, null, 2));
-
-  return removedContact;
-};
-
-const addContact = async (body) => {
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
-    email: Joi.string()
-      .email({
-        minDomainSegments: 2,
-        tlds: { allow: ["com", "net"] },
-      })
-      .required(),
-    phone: Joi.number().min(3).required(),
-  });
-
-  const validateResult = schema.validate(body);
-
-  if (validateResult.error) {
-    return { error: validateResult.error.details };
-  }
-
-  const newContact = {
-    id: Date.now().toString(),
-    ...body,
-  };
-
-  await db.push(newContact);
-
-  await fs.writeFile(dbPath, JSON.stringify(db, null, 2));
-
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30),
-    email: Joi.string().email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
-    }),
-    phone: Joi.number().min(3),
-  });
-
-  const validateResult = schema.validate(body);
-
-  if (validateResult.error) {
-    return { error: validateResult.error.details };
-  }
-
-  const idStringifyed = contactId.toString();
-
-  const findedIndex = await db.findIndex((el) => el.id === idStringifyed);
-
-  if (findedIndex === -1) {
-    return null;
-  }
-
-  db[findedIndex] = {
-    id: contactId,
-    ...body,
-  };
-
-  await fs.writeFile(dbPath, JSON.stringify(db, null, 2));
-
-  return db[findedIndex];
-};
+const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
@@ -98,4 +16,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-};
+}
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,25 @@
-const express = require("express");
+const express = require('express')
 
-const {
-  getAllContacts,
-  getContact,
-  createContact,
-  deleteContact,
-  changeContact,
-} = require("../../controls/controls");
+const router = express.Router()
 
-const router = express.Router();
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.get("/", getAllContacts);
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.get("/:contactId", getContact);
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.post("/", createContact);
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.delete("/:contactId", deleteContact);
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-router.put("/:contactId", changeContact);
-
-module.exports = router;
+module.exports = router
